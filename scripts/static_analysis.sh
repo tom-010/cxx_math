4a6326581e6acd04d48a7b88170ec994b37e4b59
Thomas Deniffel <tdeniffel@gmail.com>
2022-01-22 09:14:26 +0100

    static analyis: cppcheckignore

--- scripts/static_analysis.sh
+++ scripts/static_analysis.sh
@@ -7,5 +7,8 @@ cppcheck . \
     --std=c++20 \
     --quiet \
     --enable=all \
+    --suppressions-list=.cppcheckignore \
     --suppress=missingInclude \
     --error-exitcode=1 || exit 1
+
+# --project=compile_commands.json \
