3e3a2984a83b93f14f316cedd9ee0d19f40c7277
Thomas Deniffel <tdeniffel@gmail.com>
2022-01-24 17:29:14 +0100

    first testdata usage

--- main/hello_greet_test.cc
+++ main/hello_greet_test.cc
@@ -2,6 +2,9 @@
 #include "gmock/gmock.h"
 #include "main/hello_greet.h"
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 
 class MyMock {
  public:
@@ -21,4 +24,26 @@ TEST(GreetingShould, ReturnHelloWorld) {
     EXPECT_EQ("Hello tom", get_greet("tom"));
     EXPECT_EQ("Hello bob", get_greet("bob"));
     EXPECT_EQ(2, 2);
+}
+
+
+std::vector<std::string> ReadLines(std::string path) {
+    std::vector<std::string> vec;
+
+    std::ifstream file_in(path);
+    system("ls main");
+    if (!file_in) 
+        throw std::invalid_argument("file not found");
+
+    std::string line;
+    while (std::getline(file_in, line))
+    {
+        vec.emplace_back(line);
+    }
+    return vec;
+}
+
+TEST(File, Read) {
+    auto lines = ReadLines("main/testdata/somefile.txt");
+    std::cout << lines[0] << std::endl;
 }
